b3de62b0e220dd7469126e7b4e8135e3a4ffdfb9
Miguel Armando Riem de Oliveira <m.riem.oliveira@gmail.com>
2018-03-01 10:29:57 +0000

    added ro.h

--- player_moliveira/src/player_moliveira_node.cpp
+++ player_moliveira/src/player_moliveira_node.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
+#include <ros/ros.h>
+
 int main()
 {
-  std::cout << "Hello world" << std::endl;           
-  return 1;                                                      
+  std::cout << "Hello world" << std::endl;
+  return 1;
 }
