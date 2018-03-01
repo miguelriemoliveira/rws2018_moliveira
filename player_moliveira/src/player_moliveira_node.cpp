4d4288c3f1bc0a586296389636652c9edca1aa7b
Miguel Armando Riem de Oliveira <m.riem.oliveira@gmail.com>
2018-03-01 11:19:21 +0000

    cpp code with cpp functionalities player and myplayer classes

--- player_moliveira/src/player_moliveira_node.cpp
+++ player_moliveira/src/player_moliveira_node.cpp
@@ -1,9 +1,71 @@
-#include <iostream>
+#include <iostream>                                                                                     
 
-#include <ros/ros.h>
+class Player
+{
+  public:
+    Player(std::string argin_name)
+    {
+      name = argin_name;
+    }
+
+    std::string name;
+
+    //Overloaded setter for team_nam
+    int setTeamName(int index = 2)
+    {
+      if (index == 0){ setTeamName("red");}
+      else if (index == 1){ setTeamName("green");}
+      else if (index == 2){ setTeamName("blue");}
+      else { setTeamName("none");}
+    }
+
+    //Setter for team_nam
+    int setTeamName(std::string argin_team)
+    {
+      if (argin_team=="red" || argin_team=="green" || argin_team=="blue")
+      {
+        team_name = argin_team; return 1;
+      }
+      else
+      {
+        std::cout << "cannot set team name to " << argin_team << std::endl;
+        return 0;
+      }
+    }
+
+    //Getter of team_name
+    std::string getTeamName(void)
+    {
+      return team_name;
+    }
+
+  private:
+    std::string team_name;
+};
+
+
+class MyPlayer : public Player
+{
+  public:
+  MyPlayer(std::string argin_name, std::string argin_team) : Player(argin_name)
+  {
+    setTeamName(argin_team);
+  }
+
+};
+
 
 int main()
 {
-  std::cout << "Hello world" << std::endl;
-  return 1;
+  //Creating an instance of class Player
+  Player player("moliveira");
+
+  std::cout << "Created an instance of class player with public name " << player.name << std::endl;
+  player.setTeamName();
+  std::cout << "team_name = " << player.getTeamName() << std::endl;
+
+  MyPlayer my_player("moliveira", "green");
+
+  std::cout << "myplayer team_name = " << my_player.getTeamName() << std::endl;
 }
+
